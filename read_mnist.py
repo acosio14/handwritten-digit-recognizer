dcb6eaccda4d8b42460baa74faca43cc471c0477
Adrian Cosio <adriancosio14@gmail.com>
2025-11-02 05:24:58 -0600

    Add numpy functions to extract bytes into np arrays.

--- read_mnist.py
+++ read_mnist.py
@@ -5,16 +5,25 @@
 # and width, followed by the raw pixel data for each image.
 
 import os
+import matplotlib.pyplot as plt
+import numpy as np
 
 def read_idx(idx_file):
     with open(idx_file,'rb') as file:
-        magic_number = file.read(4)
-        dimension_1 = file.read(4)
-        dimension_2 = file.read(4)
-        dimension_3 = file.read(4)
+        magic_number = file.read(4) #0 0 (data type) (num of dimensions fo stored arrays)
+        dimension_1 = file.read(4) # 60000
+        dimension_2 = file.read(4) # 28
+        dimension_3 = file.read(4) # 28
         data = file.read()
-    
-    
+    data_np = np.frombuffer(data, dtype=np.ubyte) #NEED datatype
+    img_pixels = data_np.reshape(dimension_1,dimension_2,dimension_3)
+
+def show_image(image_name: str,image_rgb: tuple[int, int, int]):
+
+    plt.imshow(image_rgb)
+    plt.title(image_name)
+    plt.show()
+
 
 def main():
 
