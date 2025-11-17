69da14c17d6efef7375af9a4aa0f7f1368c5b8f3
Adrian Cosio <adriancosio14@gmail.com>
2025-11-17 06:09:10 -0600

    Add typehints and quick function docstring summary.

--- read_mnist.py
+++ read_mnist.py
@@ -8,41 +8,47 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+from typing import BinaryIO, Tuple
+from numpy.typing import NDArray
 
-def read_imgages_idx(idx_file):
+def read_imgages_idx(idx_file: BinaryIO) -> NDArray:
+    """Process .idx file input images and convert to numpy array."""
     with open(idx_file,'rb') as file:
-        magic_number = file.read(4) #0 0 (data type) (num of dimensions fo stored arrays)
-        dimension_1 = int.from_bytes(file.read(4), byteorder='big', signed=False) # 60000
-        dimension_2 = int.from_bytes(file.read(4), byteorder='big', signed=False)# 28
-        dimension_3 = int.from_bytes(file.read(4), byteorder='big', signed=False)# 28
+        magic_number = file.read(4)
+        dimension_1 = int.from_bytes(file.read(4), byteorder='big', signed=False)
+        dimension_2 = int.from_bytes(file.read(4), byteorder='big', signed=False)
+        dimension_3 = int.from_bytes(file.read(4), byteorder='big', signed=False)
         data_np = np.frombuffer(file.read(), dtype=np.uint8) #grayscale (8-bit unsigned integer)
     return data_np.reshape(dimension_1,dimension_2,dimension_3) #img pixels (img, row, column)
 
-def read_labels_idx(idx_file):
+def read_labels_idx(idx_file: BinaryIO) -> NDArray:
+    """Process .idx file labels and convert to numpy array."""
     with open(idx_file,'rb') as file:
-        magic_number = file.read(4) #0 0 (data type) (num of dimensions fo stored arrays)
+        magic_number = file.read(4)
         dimension_1 = int.from_bytes(file.read(4), byteorder='big', signed=False)
         data_np = np.frombuffer(file.read(), dtype=np.uint8) 
     return data_np.reshape(dimension_1,1) 
 
-def show_image(image_name: str,gray_img: tuple[int, int, int]):
-
+def show_image(image_name: str, gray_img: Tuple[int, int, int]) -> None:
+    """Plot image."""
     plt.imshow(gray_img)
     plt.axis('off')
     plt.title(image_name)
     plt.imshow(gray_img,cmap='gray')
 
-def standardize_data(dataset): # Might need a reverse standardize. To get back to original data
-    # Standardization of dataset
+def standardize_data(dataset: NDArray) -> NDArray: 
+    """Standardize data."""
     print(f"Standardized with mean: {np.round(np.mean(dataset),2)} and std: {np.round(np.std(dataset),2)}")
     return (dataset - np.mean(dataset)) / np.std(dataset)
 
-def normalize_data(dataset):
+def normalize_data(dataset: NDArray) -> NDArray:
+    """Normalize data."""
     dataset_min = np.min(dataset)
     dataset_max = np.max(dataset)
     return (dataset - dataset_min) / (dataset_max - dataset_min)
 
-def split(images_data, labels, val_ratio):
+def split(images_data: NDArray, labels: NDArray, val_ratio: float) -> Tuple[NDArray, NDArray, NDArray, NDArray]:
+    """Split data into train and validation sets."""
 
     number_of_images = len(labels)
     shuffled_sequence = np.random.permutation(number_of_images)
@@ -58,40 +64,14 @@ def split(images_data, labels, val_ratio):
 
     return X_train, X_val, y_train, y_val
 
-def convert_numpy_to_flatten_tensor(numpy_array):
+def convert_numpy_to_flatten_tensor(numpy_array: NDArray):
+    """Convert numpy to a flatten tensor."""
     tensor_array = torch.tensor(numpy_array,dtype=torch.float32)
     return torch.flatten(tensor_array, start_dim=1)
 
 
 def main():
-
-    mnst_dataset = {
-        'train_images': 'MNIST_dataset/train-images-idx3-ubyte/train-images-idx3-ubyte',
-        'train_labels': 'MNIST_dataset/train-labels-idx1-ubyte/train-labels-idx1-ubyte',
-        'test_images': 'MNIST_dataset/t10k-images-idx3-ubyte/t10k-images-idx3-ubyte',
-        'test_labels': 'MNIST_dataset/t10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte',
-    }
+    ...
     
-    train_images_filepath = os.path.join(os.getcwd(),mnst_dataset.get('train_images'))
-    train_labels_filepath = os.path.join(os.getcwd(),mnst_dataset.get('train_labels'))
-    test_images_filepath = os.path.join(os.getcwd(),mnst_dataset.get('test_images'))
-    test_labels_filepath = os.path.join(os.getcwd(),mnst_dataset.get('test_labels'))
-
-    train_images = read_imgages_idx(train_images_filepath)
-    train_labels = read_labels_idx(train_labels_filepath)
-    test_images = read_imgages_idx(test_images_filepath)
-    test_labels = read_labels_idx(test_labels_filepath)
-
-    X_train, X_val, y_train, y_val = split(train_images, train_labels, 0.2)
-
-    X_train = standardize_data(X_train)
-    X_val = standardize_data(X_val)
-    
-    # Need to flatten X_train before inputting in NN
-    # Need to train a NN: image pixels are my features -> prediction, 
-    # image_label is my target. Loss is pred_imag - true_label
-    # At first, predicted value can be random but with more iteration/training it will "learn"
-    # to match the labels.
-
 if __name__ == "__main__":
     main()
