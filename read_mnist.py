fd1a309eac2cbacfdf82d8c93780788b0dd88f44
Adrian Cosio <adriancosio14@gmail.com>
2025-11-17 06:41:03 -0600

    quick typehint for main.

--- read_mnist.py
+++ read_mnist.py
@@ -62,7 +62,7 @@ def convert_numpy_to_flatten_tensor(numpy_array: NDArray):
     tensor_array = torch.tensor(numpy_array,dtype=torch.float32)
     return torch.flatten(tensor_array, start_dim=1)
 
-def main():
+def main() -> None:
     ...
 
 if __name__ == "__main__":
