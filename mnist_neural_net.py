cab3cd18b1fc889c6a794fea71afe7fe5b108f03
Adrian Cosio <adriancosio14@gmail.com>
2025-11-19 06:40:01 -0600

    Add pool layer to reduce complexity of model and solve overfitting.

--- mnist_neural_net.py
+++ mnist_neural_net.py
@@ -32,12 +32,14 @@ class ImageConvNeuralNet(nn.Module):
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=3,kernel_size=(3,3))
         self.relu1 = nn.ReLU()
+        self.pool = nn.MaxPool2d(kernel_size=(2,2),stride=2)
         self.flatten1 = nn.Flatten()
-        self.output_layer = nn.Linear(2032,10) # 4 Channels x (26,26) + 4 (bias), (26,26) - no padding
+        self.output_layer = nn.Linear(2028,10) # 4 Channels x (26,26) + 4 (bias), (26,26) - no padding
     
     def forward(self,image):
         x = self.conv1(image)
         x = self.relu1(x)
+        x = self.pool(x)
         x = self.flatten1(x)
         x = self.output_layer(x)
 
